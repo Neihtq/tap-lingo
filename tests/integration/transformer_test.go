15eb2742480e05f168f84b5f7055b86f0837e2bf
Quang Thien Nguyen <q.thien.nguyen@outlook.de>
2026-06-04 23:56:04 +0200

    Refactor tests package to import clients package

--- tests/integration/transformer_test.go
+++ tests/integration/transformer_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	pbTransformer "github.com/neihtq/tap-lingo/transformer/gen/go/proto/transformer/v1"
+	pbTransformer "github.com/neihtq/tap-lingo/clients/transformer/proto/transformer/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
